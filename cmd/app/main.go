11c52f39a41984bc527d11c0da4e0e84f6c9e8fb
Kirill Kostycin <gotem100015@mail.ru>
2024-03-13 15:42:39 +0300

    fix

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,6 +4,9 @@ import (
 	"social_network_for_programmers"
 	"social_network_for_programmers/internal/Messanger"
 	"social_network_for_programmers/internal/config"
+	"social_network_for_programmers/internal/service/authentication/models"
+	"social_network_for_programmers/internal/service/authentication"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -13,7 +16,10 @@ func main() {
 	}
 
 	router := gin.New()
-
+	messageHandler := Messanger.NewMessangerHandler()
+	messageHandler.Register(router)
+	authHandler := &authentication.AuthHandler{&authentication.AuthStorage{map[int]models.User{}}}
+	router.RouterGroup.POST("/register", authHandler.Register)
 	srv := new(social_network_for_programmers.Server)
 	if err = srv.Run(cfg.HttpServer, router); err != nil {
 
